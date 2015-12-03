7410ec232fab75e1b216c704448355c13e109c52
Kooshini <kooshini@gmail.com>
2015-12-03 21:37:05 +0000

    Update permissions.rb

--- drupal/recipes/permissions.rb
+++ drupal/recipes/permissions.rb
@@ -1,8 +1,8 @@
 ## Recipe to create directories and set permissions for the Drupal files folder 
 	node[:deploy].each do |app_name, deploy|
-			directory "#{deploy[:deploy_to]}/sites/default/files" do 
+			directory "#{deploy[:deploy_to]}/current/sites/default/files" do 
 			  mode 0777
-			  owner 'apache'
+			  owner 'deploy'
 			  group 'apache'
 			  recursive true
 			  action :create
